6d99dfd3f6fd501ed9557b2973154ff3551f1396
Mahigavale <maheshgavale07@gmail.com>
2024-03-01 13:41:00 +0530

    change the vowel to adjacent captila consotant

---
+++ vowels.js
@@ -0,0 +1,29 @@
+/*
+i/p=> I eat pineapple.
+
+o/p=> J FBt pJnFBpplB.
+
+hint: replace the vowel with theadjacent consotant.
+*/
+
+let str=" i eat pineapple.";
+let arr=str.split('');
+//console.log(arr);
+
+for(let v=0;v<arr.length;v++)
+{
+    if(arr[v]=='a')
+    {
+        arr[v]='B';
+    }
+    else if(arr[v]=='e')
+    {
+        arr[v]='F';
+    }
+    else if(arr[v]=='i')
+    {
+        arr[v]=='J';
+    }
+}
+console.log("Input String:",str);
+console.log("Output string:",arr.join(''));
